0b2bc48c92e84983af4892eaf21c592bbe095c49
kasimlyee <kasiimlyee@gmail.com>
2025-12-07 11:51:12 +0300

    Environment setup

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// zkrust library
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
