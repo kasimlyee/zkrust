47d760f1daba620d935b16530d65031b6990f806
kasimlyee <kasiimlyee@gmail.com>
2025-12-07 11:41:32 +0300

    Initial Commit

---
+++ target/debug/deps/zkrust-b55a664212ee2769.d
@@ -0,0 +1,5 @@
+F:\Kasim Lyee\Projects\zkrust\target\debug\deps\zkrust-b55a664212ee2769.d: src\lib.rs
+
+F:\Kasim Lyee\Projects\zkrust\target\debug\deps\libzkrust-b55a664212ee2769.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/zkrust-f96386eda9a82275.d
@@ -0,0 +1,5 @@
+F:\Kasim Lyee\Projects\zkrust\target\debug\deps\zkrust-f96386eda9a82275.d: src\lib.rs
+
+F:\Kasim Lyee\Projects\zkrust\target\debug\deps\libzkrust-f96386eda9a82275.rmeta: src\lib.rs
+
+src\lib.rs:
